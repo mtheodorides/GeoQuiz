bcf0fab99af2bae61cc1e5de875584af5f2bec9a
Michael Theodorides <michael.theodorides@gmail.com>
2016-12-14 23:21:16 -0800

    Completed Chapter 2

---
+++ GeoQuiz/app/src/main/java/com/mtheodorides/android/geoquiz/Question.java
@@ -0,0 +1,34 @@
+package com.mtheodorides.android.geoquiz;
+
+/**
+ * Created by Michael Theodorides on 12/14/2016.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+
+        mAnswerTrue = answerTrue;
+
+    }
+}
