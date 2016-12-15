c2232addc41543237427de967a1b514f85e6d0ef
Michael Theodorides <michael.theodorides@gmail.com>
2016-12-15 01:58:02 -0800

    Completed Challenge: Add a Previous Button

--- GeoQuiz/app/src/main/java/com/mtheodorides/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/mtheodorides/android/geoquiz/QuizActivity.java
@@ -12,6 +12,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -45,7 +46,7 @@ public class QuizActivity extends AppCompatActivity {
     }
 
 
-        @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
@@ -79,6 +80,18 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mCurrentIndex = (mCurrentIndex + 1 ) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                if(mCurrentIndex < 0) {
+                    mCurrentIndex+=mQuestionBank.length;
+                }
                 updateQuestion();
             }
         });
