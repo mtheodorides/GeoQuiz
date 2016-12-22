b52a7e4edae9a9d4cab2f974b333a6566706861a
Michael Theodorides <michael.theodorides@gmail.com>
2016-12-21 20:37:15 -0800

    Completed Challenge: fix once they get bback, users can rotate QuizActivity to clear out mIsCheater

--- GeoQuiz/app/src/main/java/com/mtheodorides/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/mtheodorides/android/geoquiz/QuizActivity.java
@@ -23,6 +23,7 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final String BOOLEAN_KEY_INDEX = "b_index";
     private static final int REQUEST_CODE_CHEAT = 0;
 
     private Question[] mQuestionBank = new Question[] {
@@ -79,6 +80,7 @@ public class QuizActivity extends AppCompatActivity {
 
         if(savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mIsCheater = savedInstanceState.getBoolean(BOOLEAN_KEY_INDEX, false);
         }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -150,6 +152,7 @@ public class QuizActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putBoolean(BOOLEAN_KEY_INDEX, mIsCheater);
     }
 
     @Override
