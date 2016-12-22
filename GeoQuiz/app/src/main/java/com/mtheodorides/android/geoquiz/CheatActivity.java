c6ca1e1ba9a86fb164bbd645eb1075b2ec54b5b5
Michael Theodorides <michael.theodorides@gmail.com>
2016-12-21 22:28:19 -0800

    Completed Chapter 6

--- GeoQuiz/app/src/main/java/com/mtheodorides/android/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/mtheodorides/android/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.mtheodorides.android.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -62,6 +66,24 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 mAnswerShown = true;
                 setAnswerShownResult(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
     }
